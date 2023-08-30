6ea02e2e5c7e561286a5454325eb281f13b1ad97
Georginatatra <126148957+Georginatatra@users.noreply.github.com>
2023-08-30 17:05:32 +0100

    Add files via upload

---
+++ Try_project/project1.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def Home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/routine')
+def foo():
+    return render_template('routine.html')
+
+# @app.error_handler(404)
+# def page_not_found(e):
+#     return render_template('404.html')
+              
+if __name__=='__main__':
+    app.run(debug=True)
